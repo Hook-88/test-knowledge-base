914683c22a3a828880f0cb3c3347b687c95a6c96
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-10-17 16:13:05 -0500

    add ons collection

--- docs/.vuepress/.temp/pages/beheer/koppelingen/ons/ons-algemeen.html.js
+++ docs/.vuepress/.temp/pages/beheer/koppelingen/ons/ons-algemeen.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/paulushoekjan/repos/vue-press/test-knowledge-base/docs/.vuepress/.temp/pages/beheer/koppelingen/ons/ons-algemeen.html.vue"
-const data = JSON.parse("{\"path\":\"/beheer/koppelingen/ons/ons-algemeen.html\",\"title\":\"Ons Algemeen\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Ons Algemeen\",\"date\":\"2025-10-16T23:41:06.592Z\",\"tags\":[\"ons\",\"nedap\"],\"type\":\"web\"},\"git\":{},\"filePathRelative\":\"beheer/koppelingen/ons/ons-algemeen.md\"}")
+const data = JSON.parse("{\"path\":\"/beheer/koppelingen/ons/ons-algemeen.html\",\"title\":\"Ons Algemeen\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Ons Algemeen\",\"date\":\"2025-10-16T23:41:06.592Z\",\"tags\":[\"ons\",\"nedap\"],\"type\":\"web\"},\"git\":{\"updatedTime\":1760735186000,\"contributors\":[{\"name\":\"Hook-88\",\"username\":\"Hook-88\",\"email\":\"82540853+Hook-88@users.noreply.github.com\",\"commits\":1,\"url\":\"https://github.com/Hook-88\"}],\"changelog\":[{\"hash\":\"fc01107d242b97adc167de11f1b4bb9e47ee9968\",\"time\":1760735186000,\"email\":\"82540853+Hook-88@users.noreply.github.com\",\"author\":\"Hook-88\",\"message\":\"add ons connection\"}]},\"filePathRelative\":\"beheer/koppelingen/ons/ons-algemeen.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
